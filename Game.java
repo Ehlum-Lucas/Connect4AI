e93dc97318838b48e96013cbf002379d2640533b
Ehlum-Lucas <lucas.iglesia@epitech.eu>
2025-04-21 16:00:57 +0700

    feat (Game.java): add player input error handling

--- Game.java
+++ Game.java
@@ -14,12 +14,25 @@ public class Game {
     public void game() {
         while (true) {
             pos.printBoard();
-            System.out.println("Your move (1-7): ");
-            int playerMove = scanner.nextInt() - 1;
+            int playerMove = -1;
 
-            if (!pos.isValidMove(playerMove)) {
-                System.out.println("Invalid move. Try again.");
-                continue;
+            while (true) {
+                System.out.println("Your move (1-7): ");
+                if (scanner.hasNextInt()) {
+                    playerMove = scanner.nextInt() - 1;
+                    if (playerMove >= 0 && playerMove < 7) {
+                        if (pos.isValidMove(playerMove)) {
+                            break;
+                        } else {
+                            System.out.println("Invalid move. Column is full. Try again.");
+                        }
+                    } else {
+                        System.out.println("Invalid input. Please enter a number between 1 and 7.");
+                    }
+                } else {
+                    System.out.println("Invalid input. Please enter a number between 1 and 7.");
+                    scanner.next();
+                }
             }
 
             if (pos.isWinningMove(playerMove)) {
